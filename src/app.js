895279d8b6a9d3ef0af7c5ec2c7c32339c116d64
crixstn <crixstn@gmail.com>
2023-01-06 21:46:52 -0300

    feat: sign-up

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
-import express from "express"
+import express,{ json } from "express"
 import cors from "cors"
 
 const app = express()
 app.use(cors())
+app.use(json())
+
+const sign = []
+
+app.post("/sign-up", (req, res) => {
+    const user = req.body
+
+    const verifyUsername = sign.find(item => item.username == user.username)
+    if(verifyUsername){
+        return res.status(409).send("This username already exists :(")
+    }
+
+    if(!user.username || !user.avatar){
+        return res.status(422).send("Please fill in all spaces :D")
+    }
+
+    sign.push(user)
+    res.sendStatus(200)
+})
 
 app.get("/", (req, res) => {
     res.send("i am ok!")
